9091a015b0c4f9aba6021a37aaf85623dadd8a52
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2022-09-29 23:11:14 +0600

    LoginActivity-Created-With-XML

---
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.firebaseauthenticationtutorial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText etLoginEmail;
+    TextInputEditText etLoginPassword;
+    TextView tvRegisterHere;
+    Button btnLogin;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etLoginEmail = findViewById(R.id.editTextLoginEmail);
+        etLoginPassword = findViewById(R.id.editTextLoginPass);
+        tvRegisterHere = findViewById(R.id.textViewRegisterHere);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+}
