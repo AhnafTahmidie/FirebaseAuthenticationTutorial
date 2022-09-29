86e8ff5058cb80ca29c4b6d9be097f077ab6a64e
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2022-09-29 22:43:24 +0600

    MainActivity-Created-With-XMLs

--- app/src/main/java/com/example/firebaseauthenticationtutorial/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.firebaseauthenticationtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    Button btnLogOut;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        String[] memberNames = getResources().getStringArray(R.array.members_name);
+
+        listView = findViewById(R.id.listViewMemberNames);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.sample_listview, R.id.textView3, memberNames);
+        listView.setAdapter(adapter);
+
     }
 }
