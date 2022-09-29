b5c09a4ae0aa1f607fa569ac9ae84c04c300116e
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2022-09-29 23:05:49 +0600

    RegisterActivity-Created-With-XML

---
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/RegisterActivity.java
@@ -0,0 +1,38 @@
+package com.example.firebaseauthenticationtutorial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextInputEditText etRegEmail;
+    TextInputEditText etRegPassword;
+    TextView tvLoginHere;
+    Button btnRegister;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etRegEmail = findViewById(R.id.editTextRegEmail);
+        etRegPassword = findViewById(R.id.editTextRegPass);
+        tvLoginHere = findViewById(R.id.textViewLoginHere);
+        btnRegister = findViewById(R.id.btnRegister);
+    }
+}
