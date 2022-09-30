50bb906404ebd773073542fa931dfcfe155b6d44
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2022-09-30 21:47:45 +0600

    mainactivity-design-fixed

--- app/src/main/java/com/example/firebaseauthenticationtutorial/MainActivity.java
+++ app/src/main/java/com/example/firebaseauthenticationtutorial/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.firebaseauthenticationtutorial;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
